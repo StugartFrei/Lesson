9ae8ec0baf64773b8da774b03a3a25223b1a3898
StugartFrei <stugartfrei@gmail.com>
2022-11-06 17:50:57 +0300

    Calc_ajax_v2

--- calc_ajax.php
+++ calc_ajax.php
@@ -1,97 +1,46 @@
 <?php
     $connection = mysqli_connect("localhost", "root", "", "test");
     if(!$connection){
-        die("Связь не установлена:" . mysqli_connect_error());
+        die("Связь не установлена:" . mysqli_connect_error());  // проверяем наличия соединения с базой данных
     }
 
     else {
         $number_res=[];
-        $number_res = explode(",", $_REQUEST['number_arr']);
-        $number1 = (int)$number_res[0];
-        $number2 = (int)$number_res[1];
-        $operation = $number_res[2];
+        $number_res = explode(",", $_REQUEST['number_arr']);    // получаем массив из введенных значений: первое число, второе число, значение операции из строки, запятая - граница элементов
+        $number1 = (int)$number_res[0];                         // присваиваем переменной значение первого числа, преобразуем его в целое число 
+        $number2 = (int)$number_res[1];                         // присваиваем переменной значение второго числа, преобразуем его в целое число 
+        $operation = $number_res[2];                            // присваиваем переменной значение операции
 
-        if($operation == "Сложение"){
-            $number_result = $number1 + $number2;
+        if($operation == "Сложение"){                           // проверяем значение операции - операции сложения
+            $number_result = $number1 + $number2;               // выполняем сложение введенных значений
             mysqli_query($connection, "INSERT INTO `calculating_results`(`first_number`, `second_number`, `operation`, `result`) VALUES ('$number1 ','$number2','$operation','$number_result')");
-        } 
-        else if($operation == "Вычитание"){
-            $number_result = $number1 - $number2;
+        }                                                       // заносим в базу данных значения введенных значений, операции, результата
+        else if($operation == "Вычитание"){                     // проверяем значение операции - операции вычитания
+            $number_result = $number1 - $number2;               // выполняем операцию вычитание
             mysqli_query($connection, "INSERT INTO `calculating_results`(`first_number`, `second_number`, `operation`, `result`) VALUES ('$number1 ','$number2','$operation','$number_result')");
-        }
-        else if($operation == "Умножение"){
-            $number_result = $number1 * $number2;
-        }
-        else if($operation == "Деление"){
-            $number_result = $number1 / $number2;
+        }                                                       // заносим в базу данных значения введенных значений, операции, результата
+        else if($operation == "Умножение"){                     // проверяем значение операции - операции умножения
+            $number_result = $number1 * $number2;               // выполняем операцию умножения
             mysqli_query($connection, "INSERT INTO `calculating_results`(`first_number`, `second_number`, `operation`, `result`) VALUES ('$number1 ','$number2','$operation','$number_result')");
-            }
+        }                                                       // заносим в базу данных значения введенных значений, операции, результата
+        else if($operation == "Деление"){                       // проверяем значение операции - операции деления
+            $number_result = $number1 / $number2;               // выполняем операцию деления
+            mysqli_query($connection, "INSERT INTO `calculating_results`(`first_number`, `second_number`, `operation`, `result`) VALUES ('$number1 ','$number2','$operation','$number_result')");
+            }                                                   // заносим в базу данных значения введенных значений, операции, результата
 
         $query = mysqli_query($connection, "SELECT * FROM calculating_results ORDER BY created DESC LIMIT 7");
-        $results = [];
+        $results = [];                                          // получаем из базы данных занчения последних 7 операций в обратном порядке
         while($row = mysqli_fetch_assoc($query)){
-            $results[] = implode(", ", $row);
-        }
-
-        $results = ["results" => [$results]];
+            $results[] = implode(", ", $row);                   // заносим эти значения в массив, разделяем запятой элементы 
+        };
+        $query_result1 = mysqli_query($connection, "SELECT `result` FROM `calculating_results` ORDER BY created DESC LIMIT 1"); 
+        $result1 = $query_result1 -> fetch_assoc();             // получаем и прасваиваем переменной значение в поле "Результат" последней строки в базе данных
+        $result2 = $result1["result"];
+        $results = [                                            // формируем окончательный массив для вывода: сначала результат последней операции, затем последние 7 строк из базы данных в обратном порядке
+            "query_result" => $result2,
+            "results1" => [$results]];
             
-        echo json_encode($results);
+        echo json_encode($results);                             // отправка данных на клиентскую часть
     }
-
-
-
-/*
-    $connection = mysqli_connect("localhost", "root", "", "test");
-    if(!$connection){
-        die("Связь не установлена:" . mysqli_connect_error());
-    }
-
-    if(!empty($_REQUEST['button_result']) && empty($_REQUEST['number1_input']) 
-    && empty($_REQUEST['number2_input'])){
-    $error = "Введите числа";
-    }
-    else
-
-    if(isset($_REQUEST['number1_input'], $_REQUEST['number2_input'])){
-        $operation_value = $_REQUEST['operation_value'];
-        if($operation_value == "Сложение"){
-            $number1_input = (int)$_REQUEST['number1_input'];
-            $number2_input = (int)$_REQUEST['number2_input'];
-            $number_result = $number1_input + $number2_input;
-            mysqli_query($connection, "INSERT INTO `calculating_results`(`first_number`, `second_number`, `operation`, `result`) VALUES ('$number1_input','$number2_input','$operation_value','$number_result')");
-        }
-        else if($operation_value == "Вычитание"){
-            $number1_input = (int)$_REQUEST['number1_input'];
-            $number2_input = (int)$_REQUEST['number2_input'];
-            $number_result = (int)$number1_input - (int)$number2_input;
-            mysqli_query($connection, "INSERT INTO `calculating_results`(`first_number`, `second_number`, `operation`, `result`) VALUES ('$number1_input','$number2_input','$operation_value','$number_result')");
-        }
-        else if($operation_value == "Умножение"){
-            $number1_input = (int)$_REQUEST['number1_input'];
-            $number2_input = (int)$_REQUEST['number2_input'];
-            $number_result = (int)$number1_input * (int)$number2_input;
-            mysqli_query($connection, "INSERT INTO `calculating_results`(`first_number`, `second_number`, `operation`, `result`) VALUES ('$number1_input','$number2_input','$operation_value','$number_result')");
-        }
-        else if($operation_value == "Деление"){
-                if($_REQUEST['number2_input']==0){
-                    $error = "Ошибка ввода";
-                }
-                else {
-                    $number1_input = (int)$_REQUEST['number1_input'];
-                    $number2_input = (int)$_REQUEST['number2_input'];
-                    $number_result = $number1_input / $number2_input; 
-                    mysqli_query($connection, "INSERT INTO `calculating_results`(`first_number`, `second_number`, `operation`, `result`) VALUES ('$number1_input','$number2_input','$operation_value','$number_result')");
-                  
-                }
-        }
-              
-    }
- 
-    $query = mysqli_query($connection, "SELECT * FROM calculating_results ORDER BY created DESC LIMIT 7");
-    $results = [];
-    while($row = mysqli_fetch_assoc($query)){
-        $results[] = $row;
-
-*/
 
 ?>
