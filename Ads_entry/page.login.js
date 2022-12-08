275a40e93105057c14592db2e8766214a157d4e0
StugartFrei <stugartfrei@gmail.com>
2022-12-08 21:19:29 +0300

    Enter&Register_Pages_v2

--- Ads_entry/page.login.js
+++ Ads_entry/page.login.js
@@ -1,37 +1,65 @@
 (function (app){
     app.PageLogin = {
         draw: function(){
-            let content = document.querySelector(".content");
-
-            let text = document.createElement("div");
-            text.append(document.createTextNode("Вход"));
-            text.classList.add("formName");
-
-            let emailField = document.createElement("input");
-            emailField.classList.add("formInputEmail");
-
-            let email = document.createElement("div");
-            email.append(document.createTextNode("Email"));
-            email.classList.add("formEmail");
-
-            let registerButton = document.createElement("button");
-            registerButton.classList.add("registerButton");
-            registerButton.addEventListener("click", BackToRegister);
-            registerButton.append(document.createTextNode("Зарегистрироваться"));
-
-            let passwordField = document.createElement("input");
-            passwordField.classList.add("formInputPassword");
-
-            let password = document.createElement("div");
-            password.append(document.createTextNode("Пароль"));
-            password.classList.add("formPassword");
-
-            let enterButton = document.createElement("button");
-            enterButton.classList.add("buttonEnter");
-            enterButton.addEventListener("click", goToEnter);
-            enterButton.append(document.createTextNode("Войти"));
+            let content         = document.querySelector(".content");
+            let text            = create_text();
+            let emailField      = create_email_input();
+            let email           = create_email_field();
+            let registerButton  = create_register_button();
+            let passwordField   = create_password_input();
+            let password        = create_password_field();
+            let enterButton     = create_enter_button();
 
             content.append(text, emailField, registerButton, email, passwordField, password, enterButton);
+
+            function create_text(){
+                let text = document.createElement("div");
+                text.append(document.createTextNode("Вход"));
+                text.classList.add("formName");
+                return text;
+            }
+
+            function create_email_input(){
+                let emailField = document.createElement("input");
+                emailField.classList.add("formInputEmail");
+                return emailField;
+            }
+
+            function create_email_field(){
+                let email = document.createElement("div");
+                email.append(document.createTextNode("Email"));
+                email.classList.add("formEmail");
+                return email;
+            }
+
+            function create_register_button(){
+                let registerButton = document.createElement("button");
+                registerButton.classList.add("registerButton");
+                registerButton.addEventListener("click", BackToRegister);
+                registerButton.append(document.createTextNode("Зарегистрироваться"));
+                return registerButton;
+            }
+
+            function create_password_input(){
+                let passwordField = document.createElement("input");
+                passwordField.classList.add("formInputPassword");
+                return passwordField;
+            }
+
+            function create_password_field(){
+                let password = document.createElement("div");
+                password.append(document.createTextNode("Пароль"));
+                password.classList.add("formPassword");
+                return password;
+            }
+
+            function create_enter_button(){
+                let enterButton = document.createElement("button");
+                enterButton.classList.add("buttonEnter");
+                enterButton.addEventListener("click", goToEnter);
+                enterButton.append(document.createTextNode("Войти"));
+                return enterButton;
+            }
 
             function BackToRegister(){
                 document.querySelector(".content").innerHTML = "";
--- Ads_entry/page.register.js
+++ Ads_entry/page.register.js
@@ -1,49 +1,18 @@
 (function (app){
     app.PageRegister = {
         draw: function(){
-            let content = document.querySelector(".content");
-
-            let text_register = document.createElement("div");
-            text_register.append(document.createTextNode("Регистрация"));
-            text_register.classList.add("formName");
-
-            let emailField_register = document.createElement("input");
-            emailField_register.classList.add("formInputEmail");
-
-            let email_register = document.createElement("div");
-            email_register.append(document.createTextNode("Email"));
-            email_register.classList.add("formEmail");
-
-            let phoneField_register = document.createElement("input");
-            phoneField_register.classList.add("formInputPhone");
-
-            let phone_register = document.createElement("div");
-            phone_register.append(document.createTextNode("Телефон"));
-            phone_register.classList.add("formPhone");
-
-            let passwordField_register = document.createElement("input");
-            passwordField_register.classList.add("formInputPassword");
-
-            let password_register = document.createElement("div");
-            password_register.append(document.createTextNode("Пароль"));
-            password_register.classList.add("formPassword");
-
-            let passwordField_confirm = document.createElement("input");
-            passwordField_confirm.classList.add("formInputPassword");
-
-            let password_confirm = document.createElement("div");
-            password_confirm.append(document.createTextNode("Подтверждение пароля"));
-            password_confirm.classList.add("formPassword");
-
-            let registerButton_register = document.createElement("button");
-            registerButton_register.classList.add("registerButton");
-            registerButton_register.addEventListener("click", goToRegister);
-            registerButton_register.append(document.createTextNode("Зарегистрироваться"));
-
-            let enterButton_register = document.createElement("button");
-            enterButton_register.classList.add("buttonEnter");
-            enterButton_register.addEventListener("click", BackToEntry);
-            enterButton_register.append(document.createTextNode("Войти"));
+            let content                 = document.querySelector(".content");
+            let text_register           = create_text_register();
+            let emailField_register     = create_emailField_register();
+            let email_register          = create_email_register();
+            let phoneField_register     = create_phoneField_register();
+            let phone_register          = create_phone_register();
+            let passwordField_register  = create_passwordField_register();
+            let password_register       = create_password_register();
+            let passwordField_confirm   = create_passwordField_confirm();
+            let password_confirm        = create_password_confirm();
+            let registerButton_register = create_registerButton_register();
+            let enterButton_register    = create_enterButton_register();
 
             
             content.append(text_register, emailField_register, email_register, 
@@ -53,6 +22,81 @@
 
             document.querySelector(".formName").style.marginTop = "200px";
             document.querySelector(".formName").style.marginLeft = "410px";
+
+            function create_text_register(){
+                let text_register = document.createElement("div");
+                text_register.append(document.createTextNode("Регистрация"));
+                text_register.classList.add("formName");
+                return text_register;
+            }
+
+            function create_emailField_register(){
+                let emailField_register = document.createElement("input");
+                emailField_register.classList.add("formInputEmail");
+                return emailField_register;
+            }
+
+            function create_email_register(){
+                let email_register = document.createElement("div");
+                email_register.append(document.createTextNode("Email"));
+                email_register.classList.add("formEmail");
+                return email_register;
+            }
+
+            function create_phoneField_register(){
+                let phoneField_register = document.createElement("input");
+                phoneField_register.classList.add("formInputPhone");
+                return phoneField_register;
+            }
+
+            function create_phone_register(){
+                let phone_register = document.createElement("div");
+                phone_register.append(document.createTextNode("Телефон"));
+                phone_register.classList.add("formPhone");
+                return phone_register;
+            }
+
+            function create_passwordField_register(){
+                let passwordField_register = document.createElement("input");
+                passwordField_register.classList.add("formInputPassword");
+                return passwordField_register;
+            }
+
+            function create_password_register(){
+                let password_register = document.createElement("div");
+                password_register.append(document.createTextNode("Пароль"));
+                password_register.classList.add("formPassword");
+                return password_register;
+            }
+
+            function create_passwordField_confirm(){
+                let passwordField_confirm = document.createElement("input");
+                passwordField_confirm.classList.add("formInputPassword");
+                return passwordField_confirm;
+            }
+
+            function create_password_confirm(){
+                let password_confirm = document.createElement("div");
+                password_confirm.append(document.createTextNode("Подтверждение пароля"));
+                password_confirm.classList.add("formPassword");
+                return password_confirm;
+            }
+
+            function create_registerButton_register(){
+                let registerButton_register = document.createElement("button");
+                registerButton_register.classList.add("registerButton");
+                registerButton_register.addEventListener("click", goToRegister);
+                registerButton_register.append(document.createTextNode("Зарегистрироваться"));
+                return registerButton_register;
+            }
+
+            function create_enterButton_register(){
+                let enterButton_register = document.createElement("button");
+                enterButton_register.classList.add("buttonEnter");
+                enterButton_register.addEventListener("click", BackToEntry);
+                enterButton_register.append(document.createTextNode("Войти"));
+                return enterButton_register;
+            }
 
             function BackToEntry(){
                 document.querySelector(".content").innerHTML = "";
